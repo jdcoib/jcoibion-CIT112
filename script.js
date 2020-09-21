cfbab5c55bedca4227ea524302fe56420f56053a
jdcoib <71346598+jdcoib@users.noreply.github.com>
2020-09-21 15:30:58 -0400

    Script/Index

---
+++ script.js
@@ -0,0 +1,5 @@
+var boxEle = document.body.querySelector(".box");
+var input1 = Number(prompt("Enter a number:"));
+var input2 = Number(prompt("Enter a second number:"));
+var output = input1 + input2;
+boxEle.innerHTML = output;
