afff4ebc99aece53c5233940d8327bf4cd9f7b2b
jdcoib <71346598+jdcoib@users.noreply.github.com>
2020-11-18 22:32:52 -0500

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-var boxEle = document.body.querySelector(".box");
-var input1 = Number(prompt("Enter a number:"));
-var input2 = Number(prompt("Enter a second number:"));
-var output = input1 + input2;
-boxEle.innerHTML = output;
