7fca2ef3555addad7f3ad048775cd2f701a92069
jdcoib <71346598+jdcoib@users.noreply.github.com>
2020-11-18 22:33:33 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function FadeIn() {
+    var p1 = document.getElementById('p1');
+    p1.style.transition = "opacity 1s";
+    p1.style.opacity = 1;
+    
+    var img1 = document.getElementById('img1');
+    img1.style.transition = "opacity 1s";
+    img1.style.opacity = 1;
+    img1.style.transitionDelay = "0.3s"; 
+    
+    var p2 = document.getElementById('p2');
+    p2.style.transition = "opacity 1s";
+    p2.style.opacity = 1;
+    p2.style.transitionDelay = "0.6s";
+    
+    var img2 = document.getElementById('img2');
+    img2.style.transition = "opacity 1s";
+    img2.style.opacity = 1;
+    img2.style.transitionDelay = "0.9s"; 
+    
+    var p3 = document.getElementById('p3');
+    p3.style.transition = "opacity 1s";
+    p3.style.opacity = 1;
+    p3.style.transitionDelay = "1.2s";
+    
+    var img3 = document.getElementById('img3');
+    img3.style.transition = "opacity 1s";
+    img3.style.opacity = 1;
+    img3.style.transitionDelay = "1.5s";
+}
